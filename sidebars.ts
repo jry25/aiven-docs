6a89fef0859c7f2281f4ae6140785525655574ec
Harshini Rangaswamy <harshini.rangaswamy@aiven.io>
2024-09-27 17:17:28 +0200

    update: Governance enablement flow and selection steps

--- sidebars.ts
+++ sidebars.ts
@@ -810,6 +810,7 @@ const sidebars: SidebarsConfig = {
                   },
                   items: [
                     'products/kafka/howto/enable-governance',
+                    'products/kafka/howto/governance-method',
                     'products/kafka/howto/claim-topic',
                     {
                       type: 'category',
